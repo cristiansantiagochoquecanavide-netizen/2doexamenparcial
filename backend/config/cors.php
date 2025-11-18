e7517b16902bc5ee523d629d8f2a1f4d30df3356
cristiansantiagochoquecanavide <cristiansantiagochoquecanavide@gmail.com>
2025-11-18 01:14:56 -0400

    chore: commit y push de cambios recientes en cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,8 +19,8 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://exam-2-si-1.vercel.app',
         'https://exam-2-si-1-jasb.vercel.app',
+        'http://localhost:5173',
     ],
 
     'allowed_origins_patterns' => [],
